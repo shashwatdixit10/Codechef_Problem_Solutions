c7f7f97723cf308ff2526030a443b9439465e475
Shashwat Dixit <75736156+shashwatdixit10@users.noreply.github.com>
2022-11-05 21:17:09 +0530

    Create Max_minus_Min.cpp

---
+++ Max_minus_Min.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    int A,B,C;
+    for(int i=0;i<t;i++){
+        cin>>A>>B>>C;
+        cout<<C-A<<endl;
+    }
+	return 0;
+}
