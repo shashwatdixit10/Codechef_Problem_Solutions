b8e799fd5220fd6902dde8a2c1f6129f0760b25c
Shashwat Dixit <75736156+shashwatdixit10@users.noreply.github.com>
2022-11-15 22:48:48 +0530

    Create Programming_Languages.cpp

---
+++ Programming_Languages.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t,a,b,a1,b1,a2,b2;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        cin>>a>>b>>a1>>b1>>a2>>b2;
+        if((a==a1 || a==b1) && (b==a1 || b==b1)){
+            cout<<1<<endl;
+        }
+        else if((a==a2 || a== b2) && (b==a2 || b==b2)){
+            cout<<2<<endl;
+        }
+        else{
+            cout<<0<<endl;
+        }
+    }
+    
+}
